6ff602ee8c9080dfdcdabfd981635a3ba5d81a0a
Rohit Pal <RohitPal@gotechark.com>
2024-05-12 12:39:10 +0530

    first chane

---
+++ resources/views/site/index.blade.php
@@ -0,0 +1,7 @@
+@extends('site.layout')
+
+@section('title', 'Index')
+
+@section('content')
+<h1>Index</h1>
+@endSection
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::get('/', function () {
+    return view('site.index');
+});
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+  });
